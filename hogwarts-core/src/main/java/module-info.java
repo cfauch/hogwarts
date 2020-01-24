3d0bfad5f34a83a8a16181f31b049fb82e186faa
Claire Fauch <58850236+cfauch@users.noreply.github.com>
2020-01-24 11:32:46 +0100

    standard types implementation.

--- hogwarts-core/src/main/java/module-info.java
+++ hogwarts-core/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.code.fauch.hogwarts.core {
     requires transitive java.desktop;
     exports com.code.fauch.hogwarts.core.spi;
     exports com.code.fauch.hogwarts.core.model;
+    exports com.code.fauch.hogwarts.core.model.clock;
     uses com.code.fauch.hogwarts.core.spi.IContentTypeProvider;
     uses com.code.fauch.hogwarts.core.spi.ITypeProvider;
+    provides com.code.fauch.hogwarts.core.spi.ITypeProvider with com.code.fauch.hogwarts.core.model.StdType;
 }
